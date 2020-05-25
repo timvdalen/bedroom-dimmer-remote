13fa1b107c76aa708552f0c2fc9657e27eba655c
Tim van Dalen <Tim@timvdalen.nl>
2020-05-25 13:10:14 +0200

    Add call to dimmer service

--- app/src/main/java/nl/timvdalen/bedroom_dimmer_remote/MainActivity.kt
+++ app/src/main/java/nl/timvdalen/bedroom_dimmer_remote/MainActivity.kt
@@ -2,18 +2,28 @@ package nl.timvdalen.bedroom_dimmer_remote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
-
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
 		val dimButton = findViewById<Button>(R.id.button)
 		dimButton.setOnClickListener {
-			Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+			val queue = Volley.newRequestQueue(this)
+
+			val dimRequest = StringRequest(Request.Method.GET, getString(R.string.host),
+				Response.Listener<String> {r -> Log.i("bedroom-dimmer", r)},
+				Response.ErrorListener {e -> Log.e("bedroom-dimmer", e.toString())})
+
+			queue.add(dimRequest)
 		}
 	}
 }
